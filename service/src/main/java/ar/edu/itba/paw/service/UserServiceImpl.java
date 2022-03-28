71215384bb928846478f1a318b0e5e566eaf8be3
msambartolomeo <msambartolomeo@itba.edu.ar>
2022-03-28 19:45:58 -0300

    Added persistance (db)

--- service/src/main/java/ar/edu/itba/paw/service/UserServiceImpl.java
+++ service/src/main/java/ar/edu/itba/paw/service/UserServiceImpl.java
@@ -1,20 +1,22 @@
 package ar.edu.itba.paw.service;
 
+import ar.edu.itba.paw.interfaces.UserDao;
 import ar.edu.itba.paw.interfaces.UserService;
-import ar.edu.itba.paw.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final User user;
+    private final UserDao user;
 
-    UserServiceImpl() {
-        user = new User("PAW");
+    @Autowired
+    UserServiceImpl(final UserDao userDao) {
+        this.user = userDao;
     }
 
     @Override
     public String getUsername() {
-        return user.getName();
+        return user.getUsername();
     }
 }
