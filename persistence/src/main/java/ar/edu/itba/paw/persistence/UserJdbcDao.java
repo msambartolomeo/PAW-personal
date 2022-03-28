c81e2bc8ff603ba1758d053b5aa284e46a930eaa
msambartolomeo <msambartolomeo@itba.edu.ar>
2022-03-28 20:18:36 -0300

    added JDBC (maven and basic configs)

--- persistence/src/main/java/ar/edu/itba/paw/persistence/UserJdbcDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/UserJdbcDao.java
@@ -2,10 +2,18 @@ package ar.edu.itba.paw.persistence;
 
 import ar.edu.itba.paw.interfaces.UserDao;
 import ar.edu.itba.paw.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
 
 @Repository
 public class UserJdbcDao implements UserDao {
+
+    @Autowired
+    public UserJdbcDao(final DataSource ds){
+        // TODO ds
+    }
 
     @Override
     public User getUserById(long id) {
--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -3,10 +3,13 @@ package ar.edu.itba.paw.webapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import javax.sql.DataSource;
 
 @ComponentScan({ "ar.edu.itba.paw.webapp.controller", "ar.edu.itba.paw.service", "ar.edu.itba.paw.persistence" })
 @EnableWebMvc
@@ -21,5 +24,17 @@ public class WebConfig {
         vr.setSuffix(".jsp");
 
         return vr;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        final SimpleDriverDataSource ds = new SimpleDriverDataSource();
+
+        ds.setDriverClass(org.postgresql.Driver.class);
+        ds.setUrl("jdbc:postgresql://localhost/paw");
+        ds.setUsername("root");
+        ds.setPassword("root");
+
+        return ds;
     }
 }
