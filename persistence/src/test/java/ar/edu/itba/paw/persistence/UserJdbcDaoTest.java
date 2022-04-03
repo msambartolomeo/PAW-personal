53b9ce9379da26adec3231f04ba2c81ae5683a01
msambartolomeo <msambartolomeo@itba.edu.ar>
2022-04-03 17:04:57 -0300

    added junit

---
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/UserJdbcDaoTest.java
@@ -0,0 +1,24 @@
+package ar.edu.itba.paw.persistence;
+
+import org.junit.Test;
+
+public class UserJdbcDaoTest {
+
+    @Test
+    public void testUserDoesntExist() {
+        //1. setup
+
+        //2. execution (1 method)
+
+        //3. assertion
+    }
+
+    @Test
+    public void testUserExists() {
+        //1. setup
+
+        //2. execution (1 method)
+
+        //3. assertion
+    }
+}
