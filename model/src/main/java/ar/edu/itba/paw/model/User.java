442344f524ee8a56daf4fa8c803308c175c61dd9
msambartolomeo <msambartolomeo@itba.edu.ar>
2022-05-15 21:24:54 -0300

    Added hibernate userdao

--- model/src/main/java/ar/edu/itba/paw/model/User.java
+++ model/src/main/java/ar/edu/itba/paw/model/User.java
@@ -1,12 +1,27 @@
 package ar.edu.itba.paw.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
-    private final long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_userid_seq")
+    @SequenceGenerator(allocationSize = 1, name = "users_userid_seq", sequenceName = "users_userid_seq")
+    private Long userId;
+    @Column(length = 100, nullable = false, unique = true)
     private final String username;
+    @Column(length = 100, nullable = false)
     private final String password;
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Deprecated
     public User(long id, String username, String password) {
-        this.id = id;
+        this.userId = id;
         this.username = username;
         this.password = password;
     }
@@ -19,7 +34,11 @@ public class User {
         return password;
     }
 
-    public long getId() {
-        return id;
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 }
---
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/UserJpaDao.java
@@ -0,0 +1,46 @@
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.interfaces.UserDao;
+import ar.edu.itba.paw.model.User;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class UserJpaDao implements UserDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public Optional<User> getUserById(long id) {
+        return Optional.ofNullable(em.find(User.class, id));
+    }
+
+    @Override
+    public User create(String username, String password) {
+        final User user = new User(username, password);
+        em.persist(user);
+        return null;
+    }
+
+    @Override
+    public List<User> getAll(int page) {
+        final TypedQuery<User> query = em.createQuery("FROM User as u", User.class);
+        query.setFirstResult(page * 10);
+        query.setMaxResults(10);
+        return query.getResultList();
+    }
+
+    @Override
+    public Optional<User> findByName(String name) {
+        final TypedQuery<User> query = em.createQuery("FROM User as u WHERE u.username = :username", User.class);
+        query.setParameter("username", name);
+        // return query.getResultList().stream().findFirst();
+        return Optional.ofNullable(query.getSingleResult());
+    }
+}
